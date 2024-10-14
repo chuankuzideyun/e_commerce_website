27433ca2aff8dc190fc45df028991e1b7d14a970
chuankuzideyun <yn88071@163.com>
2024-10-14 16:00:11 +0200

    describe products

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -28,7 +28,26 @@ const ProductDisplay = (props) => {
           <img src={star_dull_icon} alt="" />
           <p>(122)</p>
         </div>
-        
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-price-old">${product.old_price}</div>
+          <div className="productdisplay-right-price-new">${product.new_price}</div>
+        </div>
+        <div className="productdisplay-right-description">
+          productdisplay-right-description, productdisplay-right-description, productdisplay-right-description
+        </div>
+        <div className="productdisplay-right-size">
+          <h1>Select Size</h1>
+          <div className="productdisplay-right-sizes">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button>ADD TO CART</button>
+        <p className='productdisplay-right-category'><span>Category: </span>Women, T-Shirt, Crop Top</p>
+        <p className='productdisplay-right-category'><span>Tags: </span>Modern, Latest</p>
       </div>
     </div>
   )
