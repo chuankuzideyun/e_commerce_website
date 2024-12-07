3577caef76b91cc87edd0a186d01581287dcfcfc
chuankuzideyun <yn88071@163.com>
2024-12-07 15:15:00 +0100

    Finish the back-end

---
+++ admin/src/Components/Nav/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './navbar.css'
+import navlogo from '../../assets/nav-logo.svg'
+
+const Nav = () => {
+  return (
+    <div className='navbar'>
+      <img src="" alt="" className="nav-logo" />
+    </div>
+  )
+}
+
+export default Nav
